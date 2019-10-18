80a481f6e61b8de27a9c484f122a8e75b552c147
Saurabh singh <2908aman@gmail.com>
2019-10-18 20:49:09 +0530

    Declaration of dataTypes

---
+++ TypeScript/Types.js
@@ -0,0 +1,8 @@
+var a;
+a = 10;
+var b;
+b = true;
+//b=10 is not assignable because its boolean but in javascript you can do this so that's
+//the problem with java script.
+var c;
+c = "saurabh singh";
---
+++ TypeScript/Types.ts
@@ -0,0 +1,15 @@
+var a:number;
+
+a=10;
+
+var b:boolean;
+
+b=true;
+//b=10 is not assignable because its boolean but in javascript you can do this so that's
+//the problem with java script.
+var c:string;
+
+c="saurabh singh";
+
+
+
